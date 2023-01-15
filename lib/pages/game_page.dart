1f995ad724dcde85df3f6addc8383ddbbd72e1d2
muradhossin <murad15-2547@diu.edu.bd>
2023-01-15 19:40:12 +0600

    dice rolling

--- lib/pages/game_page.dart
+++ lib/pages/game_page.dart
@@ -1,14 +1,59 @@
+import 'package:dice_game/providers/game_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class GamePage extends StatelessWidget {
+class GamePage extends StatefulWidget {
   const GamePage({Key? key}) : super(key: key);
   static const String routeName = "/gamepage";
 
+  @override
+  State<GamePage> createState() => _GamePageState();
+}
+
+class _GamePageState extends State<GamePage> {
+  late GameProvider gameProvider;
+  @override
+  void didChangeDependencies() {
+    gameProvider = Provider.of<GameProvider>(context, listen: true);
+    super.didChangeDependencies();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Dice Game'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Image.asset(
+                  gameProvider.diceList[gameProvider.index1],
+                  width: 150,
+                  height: 150,
+                ),
+                Image.asset(
+                  gameProvider.diceList[gameProvider.index2],
+                  width: 150,
+                  height: 150,
+                ),
+              ],
+            ),
+            const SizedBox(height: 50,),
+            ElevatedButton(
+              onPressed: (){
+                setState(() {
+                  gameProvider.rollTheDices();
+                });
+
+              },
+              child: const Text('Roll the Dices', style: TextStyle(fontSize: 20),),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/providers/game_provider.dart
+++ lib/providers/game_provider.dart
@@ -1,5 +1,21 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class GameProvider extends ChangeNotifier{
+  final diceList = [
+    "images/dice-six-faces-one.png",
+    "images/dice-six-faces-two.png",
+    "images/dice-six-faces-three.png",
+    "images/dice-six-faces-four.png",
+    "images/dice-six-faces-five.png",
+    "images/dice-six-faces-six.png",
+  ];
+  int index1 = 0, index2 = 0;
+  Random random = Random.secure();
 
+  rollTheDices(){
+    index1 = random.nextInt(6);
+    index2 = random.nextInt(6);
+  }
 }
